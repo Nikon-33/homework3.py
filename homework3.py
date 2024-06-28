6a60c066f01fec908c84be2166627085c33ecd82
Nikon-33 <89101717800@mail.ru>
2024-06-28 18:24:59 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = "Ilya"
+print("Name:" + name, type(name))
+age = 25
+print("Age:", age, type(age))
+age = age + 1
+print("New age:", age, type(age))
+is_student = True
+print("Is student:", is_student, type(is_student))
